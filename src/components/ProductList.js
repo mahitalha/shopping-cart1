2b1e47dadf0b6fbb8c7ec475e71528af27aaf7c4
mahitalha <138682272+mahitalha@users.noreply.github.com>
2023-07-28 16:07:54 +0530

    Add files via upload

---
+++ src/components/ProductList.js
@@ -0,0 +1,27 @@
+import '../App.css';
+
+function ProductList({ product,addToCart}){
+    return(
+        <div className='flex'>
+       {
+        product.map((productItem,productIndex)=>{
+            return(
+                <div style={{width: '50%'}}>
+                   <div className='product-item'> 
+                    <img src={productItem.url } width="100%" />
+                   <p>{productItem.name}| {productItem.category}</p>
+                   <p>{productItem.seller}</p>
+                   <p>{productItem.price}/-</p>
+                   <button onClick={()=> addToCart(productItem)}>Add to Cart</button>
+                    </div>
+                </div>
+            )
+
+        })
+
+        }
+       
+        </div>
+    )
+}
+export default ProductList;
